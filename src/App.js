9dbced08e9561ff96091319bd182b7f19f024ee1
Shannon Nabors <shannon.nabors@gmail.com>
2020-07-21 15:33:46 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {flags: []}
+  }
+
+  componentDidMount() {
+    fetch('https://deployment-test-backend-042020.herokuapp.com/flags')
+    .then(res => res.json())
+    .then(flags => this.setState({flags: flags}))
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="header">My Favorite Flags</div>
+        <div className="gallery">
+          {this.state.flags.map(flag => {
+            return (
+              <div key={flag.id} className="card">
+                <img src={flag.image}></img>
+                <p>{flag.country}</p>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
